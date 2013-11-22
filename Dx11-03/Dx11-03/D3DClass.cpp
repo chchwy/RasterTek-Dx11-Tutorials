e91d54f6d04508641e8ca6ac0b721e7fa9543952
Matt Chang <chchwy@gmail.com>
2013-11-22 18:43:39 +0800

    Initialize D3D11 Device and Swap chain

--- Dx11-03/Dx11-03/D3DClass.cpp
+++ Dx11-03/Dx11-03/D3DClass.cpp
@@ -6,17 +6,91 @@
 #pragma comment(lib, "d3dx11.lib")
 
 
-D3DClass::D3DClass() 
-{
-}
+#define SAFE_RELEASE(x) if(x) { x->Release(); x = NULL; }
 
+
+D3DClass::D3DClass() :
+m_pDevice( nullptr ),
+m_pContext( nullptr ),
+m_pSwapChain( nullptr ),
+m_pRenderTargetView( nullptr ),
+m_pDepthStencilBuffer( nullptr ),
+m_pDepthStencilState( nullptr ),
+m_pDepthStencilView( nullptr ),
+m_pRasterState( nullptr )
+{
+
+}
 
 D3DClass::~D3DClass()
 {
 }
 
-bool D3DClass::Initialize( int, int, bool, HWND, bool, float, float )
+bool D3DClass::Initialize( int screenWidth, int screenHeight,
+                           bool bVsync,
+                           HWND hWnd,
+                           bool bFullScreen,
+                           float screenDepth,
+                           float screenNear )
 {
+
+    // Create swap chain, device and device context
+    UINT createDeviceFlags = D3D11_CREATE_DEVICE_DEBUG;
+
+    D3D_FEATURE_LEVEL myFeatureLevel;
+
+    DXGI_SWAP_CHAIN_DESC sd;
+    sd.BufferDesc.Width = screenWidth;
+    sd.BufferDesc.Height = screenHeight;
+    sd.BufferDesc.RefreshRate.Numerator = 60;
+    sd.BufferDesc.RefreshRate.Denominator = 1;
+    sd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+    sd.BufferDesc.ScanlineOrdering = DXGI_MODE_SCANLINE_ORDER_UNSPECIFIED;
+    sd.BufferDesc.Scaling = DXGI_MODE_SCALING_UNSPECIFIED;
+
+    sd.SampleDesc.Count = 1;
+    sd.SampleDesc.Quality = 0;
+
+    sd.BufferCount  = 1;
+    sd.BufferUsage  = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+    sd.OutputWindow = hWnd;
+    sd.Windowed     = !bFullScreen;
+    sd.SwapEffect   = DXGI_SWAP_EFFECT_DISCARD;
+    sd.Flags        = 0;
+
+    HRESULT hr = D3D11CreateDeviceAndSwapChain( NULL,  // Default Adapter
+                                                D3D_DRIVER_TYPE_HARDWARE,
+                                                NULL,  // No software device
+                                                createDeviceFlags,
+                                                0, 0,  // Use default feature list
+                                                D3D11_SDK_VERSION,
+                                                &sd,
+                                                &m_pSwapChain, 
+                                                &m_pDevice,
+                                                &myFeatureLevel,
+                                                &m_pContext );
+
+    if ( FAILED( hr ) )
+    {
+        return false;
+    }
+
+    if ( myFeatureLevel != D3D_FEATURE_LEVEL_11_0 )
+    {
+        return false;
+    }
+
+    // Set back buffer and render target
+    ID3D11Texture2D* pBackBuffer = NULL;
+    m_pSwapChain->GetBuffer( 0, __uuidof(ID3D11Texture2D), (void**)&pBackBuffer);
+
+    hr = m_pDevice->CreateRenderTargetView( pBackBuffer, NULL, &m_pRenderTargetView );
+    if ( FAILED( hr ) )
+    {
+        return false;
+    }
+    SAFE_RELEASE( pBackBuffer );
+
     return true;
 }
 
@@ -31,6 +105,26 @@ void D3DClass::BeginScene( float, float, float, float )
 }
 
 void D3DClass::EndScene()
+{
+
+}
+
+void D3DClass::GetProjectionMatrix( XMMATRIX& )
+{
+
+}
+
+void D3DClass::GetWorldMatrix( XMMATRIX& )
+{
+
+}
+
+void D3DClass::GetOrthoMatrix( XMMATRIX& )
+{
+
+}
+
+void D3DClass::GetVideoCardInfo( char*, int& )
 {
 
 }
--- Dx11-03/Dx11-03/D3DClass.h
+++ Dx11-03/Dx11-03/D3DClass.h
@@ -6,6 +6,9 @@
 #include <dxgi.h>
 #include <d3dcommon.h>
 #include <d3d11.h>
+#include <DirectXMath.h>
+
+using DirectX::XMMATRIX;
 
 
 class D3DClass
@@ -14,10 +17,36 @@ public:
     D3DClass();
     ~D3DClass();
 
-    bool Initialize( int, int, bool, HWND, bool, float, float );
+    bool Initialize( int screenWidth, int screenHeight, 
+                     bool bVsync, HWND hWnd, bool bFullScreen, 
+                     float screenDepth, float screenNear );
     void Shutdown();
     void BeginScene( float, float, float, float );
     void EndScene();
+
+    ID3D11Device* GetDevice() { return m_pDevice; }
+    ID3D11DeviceContext* GetDeviceContext() { return m_pContext; }
+
+    void GetProjectionMatrix( XMMATRIX& );
+    void GetWorldMatrix( XMMATRIX& );
+    void GetOrthoMatrix( XMMATRIX& );
+
+    void GetVideoCardInfo( char*, int& );
+
+private:
+    ID3D11Device*        m_pDevice;
+    ID3D11DeviceContext* m_pContext;
+    IDXGISwapChain*      m_pSwapChain;
+
+    ID3D11RenderTargetView*  m_pRenderTargetView;
+    ID3D11Texture2D*         m_pDepthStencilBuffer;
+    ID3D11DepthStencilState* m_pDepthStencilState;
+    ID3D11DepthStencilView*  m_pDepthStencilView;
+    ID3D11RasterizerState*   m_pRasterState;
+
+    XMMATRIX m_projectionMatrix;
+    XMMATRIX m_worldMatrix;
+    XMMATRIX m_orthoMatrix;
 };
 
 #endif
